314c8eefac2e10110f6070b3a70af91560147b7a
ErickDevMaster <123skatesk8@gmail.com>
2021-09-08 16:43:58 -0400

    melhorei o codigo

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const dado = [1,2,3,4,5,6]
-const dado1 = [1,2,3,4,5,6]
 let count = [['2: ',0],['3: ',0],['4: ',0],['5: ',0],['6: ',0],['7: ',0],['8: ',0],
 ['9: ',0],['10: ',0],['11: ',0],['12: ',0]]
 let num = 1000
@@ -17,8 +15,8 @@ bodyP.style.display = 'inline-block'
 bodyP.appendChild(principal)
 
 for(let i = 0 ; i < num ; i++){
-    resuD = dado[Math.floor(Math.random() * 6)]
-    resuD1 = dado1[Math.floor(Math.random() * 6)]
+    resuD = Math.floor(Math.random() * 6)+1
+    resuD1 = Math.floor(Math.random() * 6)+1
     index = (resuD + resuD1) - 2
     count[index][1] += (1)
    
@@ -27,7 +25,7 @@ criarBlocos(1)
 for(let i = 0 ; i < count.length ; i++){
     const body = document.querySelector('.Bloco1')
     const bloco = document.createElement('div')
-    bloco.style.margin = '0 15px 0 0'
+    bloco.style.margin = '0 15px 1px 0'
     bloco.innerText = count[i][0]+count[i][1]
     body.appendChild(bloco)
 }
@@ -39,29 +37,21 @@ const teste = document.querySelector('.relogar')
 teste.addEventListener('click',() => atualizar())
 console.log(count)
 
-// function criarElementos (valor){
-//     const body = document.querySelector('body')
-//     const bloco = document.createElement('div')
-//     bloco.innerText = count[i][0]+count[i][1]
-//     body.appendChild(bloco)
-//     return 
-// }
 
-// for(let i = 0; i < count.length; i++){
     criarBlocos(2)
     for(let i = 0; i < count.length; i++){
         const body = document.querySelector('.Bloco2')
         const minhaBarra = document.createElement('div')
         minhaBarra.style.width = `${count[i][1]}px`;
         minhaBarra.style.border = '5px solid red'
-        minhaBarra.style.margin = '9.5px 0 0 0'
+        minhaBarra.style.margin = '8.8px 0 0 0'
         minhaBarra.classList.add('Barra'+i);
-        // element.style.backgroundColor = 'red'
+    
         body.appendChild(minhaBarra)
     }
     const Barra0 = document.querySelector('.Barra0')
     Barra0.style.marginTop = '4px'
-// }
+
 function criarBlocos(n){
     const body = document.querySelector('.principal')
     const div = document.createElement('div')
